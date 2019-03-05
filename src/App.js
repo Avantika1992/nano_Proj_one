29bcc7048800e41e545fa6f2bbde6281bf3e4836
avantika <avireddy54@gmail.com>
2019-03-05 18:34:45 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,124 +3,16 @@ import {Router,Route} from 'react-router';
 import './App.css';
 import {getAll,get,update,search} from './BooksAPI'
 import Search from './search'
-import Parent from './parent'
 
 class BooksApp extends React.Component {
-  constructor(props) {
-        super(props);
-
-        this.state = {
-        currentlyReading:[],
-        wantToRead:[],
-        read:[],
-        availableBooks:[],
-        search:"",
-        buks:[],
-        value: ""
-       };
-
-       getAll().then((result) =>{
-       this.setState({buks:result})
-       var curr = [];
-       var want = [];
-       var toread= [];
-
-         result.forEach(function(book){
-
-         if(book.shelf==="currentlyReading")
-         {
-           curr.push(book)
-         }
-         if(book.shelf==="wantToRead")
-         {
-           want.push(book)
-         }
-         if(book.shelf==="read")
-         {
-           toread.push(book)
-         }
-       }
-
-       );
-       this.setState({currentlyReading:curr})
-       this.setState({wantToRead:want})
-       this.setState({read:toread})
-
-     }
-     )
-     }
-
-
-  handleSelectionChanged(book,placeBook,event) {
-
-    event.preventDefault()
-    if(event.target.value==="currentlyReading"){
-        book.shelf="currentlyReading"
-        var a = this.state.currentlyReading
-      	a.push(book)
-         this.setState({currentlyReading: a});
-    }
-    if(event.target.value==="wantToRead"){
-        book.shelf="wantToRead"
-        var e = this.state.wantToRead
-      	e.push(book)
-            this.setState({wantToRead: e});
-    }
-    if(event.target.value==="read"){
-        book.shelf="read"
-        var f = this.state.read
-      	f.push(book)
-        this.setState({read: f});
-    }
-     if(event.target.value==="None"){
-         book.shelf="None"
-         this.setState({availableBooks: this.state.availableBooks.push(book)});
-    }
-     if(placeBook==="currentlyReading"){
-         for( var m = 0; m < this.state.currentlyReading.length; m++){
-         if ( this.state.currentlyReading[m].title === book.title) {
-           var g=this.state.currentlyReading
-           g.splice(m, 1)
-           this.setState({currentlyReading:g});
-         console.log(g)
-        }
-      }
-    }
-      if(placeBook==="wantToRead"){
-         for( var n = 0; n < this.state.wantToRead.length; n++){
-         if ( this.state.wantToRead[n].title === book.title) {
-         const h=this.state.wantToRead
-         h.splice(n, 1)
-         this.setState({wantToRead:h});
-        }
-      }
-    }
-      if(placeBook==="read"){
-         for( var o = 0; o < this.state.read.length; o++){
-         if ( this.state.read[o].title === book.title) {
-         const z=this.state.read
-         z.splice(o, 1)
-         this.setState({read:z});
-        }
-      }
-    }
-     if(placeBook===this.state.availableBooks){
-         for( var i = 0; i < this.state.availableBooks.length; i++){
-         if ( this.state.availableBooks[i] === book.title) {
-         this.state.availableBooks.splice(i, 1);
-         console.log(book.title)
-        }
-      }
-    }
-  }
   render() {
 
-    var allBooks={"currentlyReading":this.state.currentlyReading,
-             "wantToRead":this.state.wantToRead,
-             "read":this.state.read}
+    var allBooks={"currentlyReading":this.props.currentlyReading,
+             "wantToRead":this.props.wantToRead,
+             "read":this.props.read}
     //console.log(allBooks)
     console.log(this.props.test)
-    console.log(this.props.val)
+    console.log(this.props.currentlyReading)
     //console.log(this.props.currentlyReading)
     let output = [];
      output.push(
@@ -142,7 +34,7 @@ class BooksApp extends React.Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193,backgroundImage: 'url('+allBooks[key][i].imageLinks.thumbnail+ ')'}}></div>
                             <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={this.handleSelectionChanged.bind(this,book,placeBook)}>
+                              <select value={book.shelf} onChange={this.props.handleSelectionChanged.bind(this,book,placeBook)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
--- src/search.js
+++ src/search.js
@@ -2,123 +2,20 @@ import React from 'react';
 import {Router,Route} from 'react-router';
 import './App.css';
 import {getAll,get,update,search} from './BooksAPI'
-import Parent from './parent'
+
 
 var displaySearchBooks=[];
 var displaySearch=[]
 
 
 class Search extends React.Component {
-  constructor(props) {
-        super(props);
 
-        this.state = {
-
-        currentlyReading:[],
-        wantToRead:[],
-        read:[],
-        availableBooks:[],
-        search:"",
-        buks:[],
-        value: "",
-        displaySearch:[]
-
-       };
-
-       getAll().then((result) =>{
-       this.setState({buks:result})
-       var curr = [];
-       var want = [];
-       var toread= [];
-
-         result.forEach(function(book){
-
-         if(book.shelf==="currentlyReading")
-         {
-           curr.push(book)
-         }
-         if(book.shelf==="wantToRead")
-         {
-           want.push(book)
-         }
-         if(book.shelf==="read")
-         {
-           toread.push(book)
-         }
-       }
-
-       );
-       this.setState({currentlyReading:curr})
-       this.setState({wantToRead:want})
-       this.setState({read:toread})
-
-     }
-     )
-     }
-
-     handleSelectionSearch(bookSearch,event) {
-        if(this.state.currentlyReading){
-          for( var i = 0; i < this.state.currentlyReading.length; i++){
-          if ( this.state.currentlyReading[i].title === bookSearch.title) {
-            var a=this.state.currentlyReading
-            a.splice(i, 1)
-            this.setState({currentlyReading:a});
-
-         }
-       }
-     }
-       if(this.state.wantToRead){
-          for( var j = 0; j < this.state.wantToRead.length; j++){
-          if ( this.state.wantToRead[j].title === bookSearch.title) {
-          const b=this.state.wantToRead
-          b.splice(j, 1)
-          this.setState({wantToRead:b});
-         }
-       }
-     }
-       if(this.state.read){
-          for( var k = 0; k < this.state.read.length; k++){
-          if ( this.state.read[k].title === bookSearch.title) {
-          const c=this.state.read
-          c.splice(k, 1)
-          this.setState({read:c});
-         }
-       }
-     }
-
-     if(event.target.value==="currentlyReading"){
-         bookSearch.shelf="currentlyReading"
-         var d = this.state.currentlyReading
-       	d.push(bookSearch)
-          this.setState({currentlyReading: d});
-     }
-     if(event.target.value==="wantToRead"){
-         bookSearch.shelf="wantToRead"
-         var e = this.state.wantToRead
-       	e.push(bookSearch)
-             this.setState({wantToRead: e});
-     }
-     if(event.target.value==="read"){
-         bookSearch.shelf="read"
-         var f = this.state.read
-       	f.push(bookSearch)
-             this.setState({read: f});
-     }
-     if(event.target.value==="None"){
-       bookSearch.shelf="None"
-       for( var i = 0; i <= this.state.buks.length; i++){
-       if ( this.state.buks[i] === bookSearch.title) {
-       this.state.buks.splice(i, 1);
-     }
-     }
- }
- }
  searchHandler(event){
   event.preventDefault()
   var entered_value = event.target.value
   //var arrbook=this.state.currentlyReading.concat(this.state.wantToRead)
   //arrbook=arrbook.concat(this.state.read)
-  var arrbook=this.state.buks
+  var arrbook=this.props.buks
   console.log(arrbook)
 
   this.setState({search: event.target.value});
@@ -149,7 +46,7 @@ render(){
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:'url('+ bookSearch.imageLinks.smallThumbnail + ')' }}></div>
                           <div className="book-shelf-changer">
-                            <select value={bookSearch.shelf} onChange={this.handleSelectionSearch.bind(this,bookSearch)}>
+                            <select value={bookSearch.shelf} onChange={this.props.handleSelectionSearch.bind(this,bookSearch)}>
                               <option value="move" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
